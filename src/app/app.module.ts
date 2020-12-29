077a057f7d96645b7cee01c6e84c8184867d57aa
shamineoduor <shamineoduor@gmail.com>
2020-12-29 10:36:27 +0300

    my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentdataComponent } from './studentdata/studentdata.component';
+import { StudentComponent } from './studentdata/student/student.component';
+import { StudentlistComponent } from './studentdata/studentlist/studentlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentdataComponent,
+    StudentComponent,
+    StudentlistComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/studentdata/studentdata.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-studentdata',
+  templateUrl: './studentdata.component.html',
+  styleUrls: ['./studentdata.component.css']
+})
+export class StudentdataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
